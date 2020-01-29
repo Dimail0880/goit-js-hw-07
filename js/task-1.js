634bbe289efb9552d4c8612441a827cd09b02513
dimail0880 <dimail4888@gmail.com>
2020-01-29 19:37:16 +0200

    module-7 1, 4 tasks bugs fixed

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,9 @@
-const categories = document.querySelector('#categories')
-console.log(`В списке ${categories.childElementCount} категории.`)
+const categories = document.querySelector("#categories");
+console.log(`В списке ${categories.childElementCount} категории.`);
+
+const items = document.querySelectorAll(".item");
+items.forEach.call(items, el => {
+    const title = el.querySelector("h2").innerHTML;
+    const itemsLength = el.querySelectorAll("li").length;
+    console.log(`Категория: ${title}. Количество элементов: ${itemsLength}.`);
+});
