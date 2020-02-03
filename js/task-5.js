6a108c42767864cd91b4a2a5ef6d7e3e9cccb54d
dimail0880 <dimail4888@gmail.com>
2020-02-03 10:04:35 +0200

    bug task-5 fixed

--- js/task-5.js
+++ js/task-5.js
@@ -2,5 +2,5 @@ let inputName = document.querySelector('#name-input')
 let outputName = document.querySelector('#name-output')
 
 inputName.addEventListener('input', () => {
-    return inputName.value === '' ? outputName.textContent = 'незнакомец' : outputName.textContent = inputName.value
+    inputName.value === '' ? outputName.textContent = 'незнакомец' : outputName.textContent = inputName.value
 })
